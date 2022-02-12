a48c0a7cc5a2572d5c65498c9c03c011222f7e85
yourskc <yourskc@yahoo.com.tw>
2022-02-12 22:30:24 +0800

    update

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 
 var server,
-    ip = "127.0.0.1",
-    port = 1111,
-    http = require('http'),
-    fs = require("fs"),
-	path = require("path"),
-    folderPath = ".",
-    url = require('url'),
-    path0,
-    filePath,
-    encode = "utf8";
+ip = "127.0.0.1",
+port = 1111,
+http = require('http'),
+fs = require("fs"),
+path = require("path"),
+folderPath = ".",
+url = require('url'),
+path0,
+filePath,
+encode = "utf8";
 // var spawn = require('child_process').spawn;
 
 const util = require('util');
@@ -18,98 +18,124 @@ const exec = util.promisify(require('child_process').exec);
 var mysql = require('mysql');
 
 var mime = {
-    html: 'text/html',
-    txt: 'text/plain',
-    css: 'text/css',
-    gif: 'image/gif',
-    jpg: 'image/jpeg',
-    png: 'image/png',
-    svg: 'image/svg+xml',
-    js: 'application/javascript',
-	moi: 'application/moil'
+html: 'text/html',
+txt: 'text/plain',
+css: 'text/css',
+gif: 'image/gif',
+jpg: 'image/jpeg',
+png: 'image/png',
+svg: 'image/svg+xml',
+js: 'application/javascript',
+moi: 'application/moil'
 };
 
 var con = mysql.createConnection({
-  host: "localhost",
-  user: "moil",
-  password: "********",
-  database: "moil"
+host: "localhost",
+user: "moil",
+password: "********",
+database: "moil"
 });	
 
 
 var params=function(req){
 //console.log("req="+req.url);
-  var q=req.url.split('?'),result={};
-  if(q.length>=2){
+var q=req.url.split('?'),result={};
+if(q.length>=2){
 //console.log("q[1]="+q[1]);
-      q[1].split('&').forEach((item)=>{
+  q[1].split('&').forEach((item)=>{
 //console.log("("+item+")");
-           try {
-             result[item.split('=')[0]]=item.split('=')[1];
-           } catch (e) {
-             result[item.split('=')[0]]='';
-           }
-      })
-  }
-  return result;
+       try {
+         result[item.split('=')[0]]=item.split('=')[1];
+       } catch (e) {
+         result[item.split('=')[0]]='';
+       }
+  })
 }
-	
-async function lsExample() {
-  const { stdout, stderr } = await exec('dir');
-  console.log('stdout:', stdout);
-  console.error('stderr:', stderr);
+return result;
+}
+
+
+async function exec_cli() {
+const { stdout, stderr } = await exec('./mainmoil rpi_220 1.4 1.4 1320.0 1017.0 1.048 2592 1944 3.4 0 0 0 10.11 -85.241 282.21 1 0 0 4');
+// console.log('stdout:', stdout);
+// console.error('stderr:', stderr);
 }	
 
+
+
 server = http.createServer(function(req, res) {
-    path0 = url.parse(req.url);
-	
+path0 = url.parse(req.url);
+
 switch (path0.pathname) {
-	case "/":
-        res.end('Hello Moil\n');
-	break;
-		
-		
-	case "/mysql":
+case "/":
+    res.end('Hello Moil\n');
+break;
+
+
+case "/mysql":
 
 var retStr = "" ;	
 con.connect(function(err) {
-  if (err) throw err;
-  console.log("Connected!");
-  var sql = "select * from users";
-  con.query(sql, function (err, result, fields) {
-    if (err) throw err;
-    console.log(result);  	
-  });
+if (err) throw err;
+console.log("Connected!");
+var sql = "select * from users";
+con.query(sql, function (err, result, fields) {
+if (err) throw err;
+console.log(result);  	
+});
 });	
-	
-    res.end('this is mysql page.\n');
-    break;
-	
-    default:  // 讀取檔案
-	var type = mime[path.extname(path0.pathname).slice(1)] || 'text/plain';
-    // var type = 'image/jpeg';
-    var s = fs.createReadStream(folderPath + "/" + path0.pathname);
-    s.on('open', function () {
-        res.setHeader('Content-Type', type);
-		// res.setHeader('Content-disposition', 'attachment; filename='+query.file);
-        s.pipe(res);
-    });
-    s.on('error', function () {
-        res.setHeader('Content-Type', 'text/plain');
-        res.statusCode = 404;
-        res.end('Not found');
-    });	
-	
-	
-	// res.setHeader('Content-Type', 'text/plain');
-    // res.end(  path0.pathname + '\n' );
-	//	exec('dir').unref();
-		lsExample();
-		
-        break;
-    }	
-	
-	
+
+res.end('this is mysql page.\n');
+break;
+
+
+case "/x.map":
+
+exec_cli();
+// var s = fs.createReadStream(folderPath + "/" + path0.pathname);
+var s = fs.createReadStream( "./x.map" );
+s.on('open', function () {
+res.setHeader('Content-Type', 'application/moil');
+// res.setHeader('Content-disposition', 'attachment; filename='+query.file);
+res.statusCode = 200;
+s.pipe(res);
+});
+s.on('error', function () {
+res.setHeader('Content-Type', 'text/plain');
+res.statusCode = 404;
+res.end('Error');
+});	
+
+break; 
+
+case "/y.map":
+
+// exec_cli(); // suppose x.map would be called first
+// var s = fs.createReadStream(folderPath + "/" + path0.pathname);
+var s = fs.createReadStream( "./y.map" );
+s.on('open', function () {
+res.setHeader('Content-Type', 'application/moil');
+// res.setHeader('Content-disposition', 'attachment; filename='+query.file);
+res.statusCode = 200;
+s.pipe(res);
+});
+s.on('error', function () {
+res.setHeader('Content-Type', 'text/plain');
+res.statusCode = 404;
+res.end('Error');
+});	
+
+break; 
+default:  
+
+break;
+
+
+
+
+}	
+
+
 
 });
 
